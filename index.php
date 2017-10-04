d2e914142362fbb942d68c87a54b1c020694f88b
maxim <makcfw@yandex.ru>
2017-10-04 22:04:25 +0400

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/*здесь комментарий */
+/*echo "Hello World<br>";*/
+
+//echo "privet \n";
+$c = array();
+$c[0]=12;
+$c[1]=13;
+$c[]=14;
+
+$a="Hello world";
+$b=$a;
+
+
+//echo ($c);
+var_dump($c[1]);
+?>
+
